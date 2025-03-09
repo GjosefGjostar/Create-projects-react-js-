2768933a736550e9634a59085162df0eebb103bb
GjosefGjostar <nikitagrigench@gmail.com>
2025-03-09 14:08:55 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+	return (
+		<header className='header'>
+    <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGEOjCR1mr-ukriVfbG4HAZhqFGXqcy1Oaqw&s'></img>
+   </header>
+	);
+};
+
+export default Header
